4176fed75862ec3519887ea0bab516f40e477dbc
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-10-09 19:53:50 +0200

    html file created

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const redis = require(redis)
+
+const port = 4000
+const app = express()
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+app.length('/', function(req, res, next){
+    res.render('searchusers')
+})
+
+app.listen(port, function(){
+    console.log(`Server started on port ${port}`)
+})
