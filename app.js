80e1868753973eb9d8ab98adb73d07a1a85edc14
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-10-10 08:56:55 +0200

    server running

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const path = require('path')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const redis = require(redis)
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const redis = require('redis');
 
-const port = 4000
-const app = express()
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(methodOverride('_method'))
+const port = 4000;
+const app = express();
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
-app.length('/', function(req, res, next){
-    res.render('searchusers')
+app.use('/', function(req, res, next){
+    res.render('searchusers');
 })
 
 app.listen(port, function(){
-    console.log(`Server started on port ${port}`)
+    console.log(`Server started on port ${port}`);
 })
